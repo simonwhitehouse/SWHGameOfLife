f74d0b6788eeb956342aff03b9adab2fe0dc91b1
Simon J Whitehouse <swhitehouse@me.com>
2015-10-03 22:32:40 +0100

    more work on model state

--- SWHGameOfLife/SWHGameOfLife/GameOfLifeModel.swift
+++ SWHGameOfLife/SWHGameOfLife/GameOfLifeModel.swift
@@ -41,25 +41,101 @@ struct GameOfLideModel {
     }
     
     mutating func step() {
+        var nextInteraction = livingCells
         
+        for var y = 0; y < GameOfLifeVC.NumberOfCellsPerRow; y++ {
+            for var x = 0; x < GameOfLifeVC.NumberOfCellsPerRow; x++ {
+                var cell = nextInteraction[y][x]
+                
+                
+            }
+        }
     }
     
+    func getCellNeighbours(nextInteraction: GameBoardState, cell: LivingView, x: Int, y: Int) -> NeighbourCells {
+        var topLeftCell: LivingViewState?
+        var topMiddleCell: LivingViewState?
+        var topRightCell: LivingViewState?
+        var leftCenter: LivingViewState?
+        var rightCenter: LivingViewState?
+        var bottomLeftCell: LivingViewState?
+        var bottomMiddle: LivingViewState?
+        var bottomRightCell: LivingViewState?
+        
+        if y > 0 {
+            if x > 0 {
+                var cell = nextInteraction[y-1][x-1]
+                topLeftCell = cell.currentLivingViewState
+            }
+            
+//            let nextFly = flies[y-1][x]
+//            if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                nextFly.resetTimer()
+//            }
+//            
+//            if x+1 <  SWHFireFlyViewController.NumberOfFlysPerRow  {
+//                let nextFly = flies[y-1][x+1]
+//                if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                    nextFly.resetTimer()
+//                }
+//            }
+        }
+        
+//        if y+1 < SWHFireFlyViewController.NumberOfFlysPerRow {
+//            
+//            if x+1 <  SWHFireFlyViewController.NumberOfFlysPerRow  {
+//                let nextFly = flies[y+1][x+1]
+//                if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                    nextFly.resetTimer()
+//                }
+//            }
+//            
+//            let nextFly = flies[y+1][x]
+//            if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                nextFly.resetTimer()
+//            }
+//            
+//            if x-1 > 0 {
+//                let nextFly = flies[y+1][x-1]
+//                if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                    nextFly.resetTimer()
+//                }
+//            }
+//        }
+//        
+//        if x+1 < SWHFireFlyViewController.NumberOfFlysPerRow  {
+//            let nextFly = flies[y][x+1]
+//            if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                nextFly.resetTimer()
+//            }
+//        }
+//        
+//        if x-1 > 0 {
+//            let nextFly = flies[y][x-1]
+//            if nextFly.ellapsedTimer < SWHFireFlyViewController.SensitivePeriod {
+//                nextFly.resetTimer()
+//            }
+//        }
+        
+        return (topLeftCell, topMiddleCell, topRightCell, leftCenter, rightCenter, bottomLeftCell, bottomMiddle, bottomRightCell)
+    }
+    
+    
+    typealias NeighbourCells = (topLeftCell: LivingViewState?, topMiddleCell: LivingViewState?, topRightCell: LivingViewState?, leftCenter: LivingViewState?, rightCenter: LivingViewState?, bottomLeftCell: LivingViewState?, bottomMiddle: LivingViewState?, bottomRightCell: LivingViewState?)
     typealias NeighbourCellStates = (aliveNeightbours: Int, deadNeighbours: Int)
     
-    func neighBouringCellStates(topLeftCell: LivingViewState?, topMiddleCell: LivingViewState?, topRightCell: LivingViewState?, leftCenter: LivingViewState?, rightCenter: LivingViewState?, bottomLeftCell: LivingViewState?, bottomMiddle: LivingViewState?, bottomRightCell: LivingViewState?) -> NeighbourCellStates {
+    func neighBouringCellStates(neighbours: NeighbourCells) -> NeighbourCellStates {
         
         var livingCount: Int = 0
         var deadCount: Int = 0
         
-        for neighBour in [topLeftCell, topMiddleCell, topRightCell, leftCenter, rightCenter, bottomLeftCell, bottomMiddle, bottomRightCell] {
+        for neighBour in [neighbours.topLeftCell, neighbours.topMiddleCell, neighbours.topRightCell, neighbours.leftCenter, neighbours.rightCenter, neighbours.bottomLeftCell, neighbours.bottomMiddle, neighbours.bottomRightCell] {
             
             if neighBour == LivingViewState.Alive {
                 livingCount++
             } else {
                 deadCount++
             }
-            
-            
         }
         
         
